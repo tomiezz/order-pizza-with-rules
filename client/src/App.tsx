46b0bef8a9732229b8fcf1dae3ae8ff09377e5ff
Tomiez <tom.hiepnguyen@gmail.com>
2022-10-26 18:04:48 +0700

    feat: code-splitting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,37 @@
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { CircularProgress, CssBaseline, ThemeProvider } from "@mui/material";
 import { APP_ROUTES, theme } from "./utils";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
-import Home from "./containers/Home";
-import Product from "./containers/Product";
-import Checkout from "./containers/Checkout";
-import NotFoundPage from "./containers/NotFoundPage";
-import BaseLayout from "./layouts/BaseLayout";
 import ErrorBoundary from "./layouts/ErrorBoundaries";
-import FeatureLayout from "./layouts/FeatureLayout.tsx/index";
 import { FeatureContextProvider } from "contexts/FeatureContext";
+import { lazy, Suspense } from "react";
+
+const Home = lazy(() => import("./containers/Home"));
+const Product = lazy(() => import("./containers/Product"));
+const Checkout = lazy(() => import("./containers/Checkout"));
+const NotFoundPage = lazy(() => import("./containers/NotFoundPage"));
+const BaseLayout = lazy(() => import("./layouts/BaseLayout"));
 
 function App() {
   return (
     <ErrorBoundary>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <BrowserRouter>
-          <Routes>
-            <Route path={APP_ROUTES.HOME} element={<BaseLayout />}>
-              <Route index element={<Home />} />
-              <Route path={APP_ROUTES.NOT_FOUND} element={<NotFoundPage />} />
-            </Route>
+      <Suspense fallback={<CircularProgress />}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <BrowserRouter>
+            <Routes>
+              <Route path={APP_ROUTES.HOME} element={<BaseLayout />}>
+                <Route index element={<Home />} />
+                <Route path={APP_ROUTES.NOT_FOUND} element={<NotFoundPage />} />
+              </Route>
 
-            <Route element={<FeatureContextProvider />}>
-              <Route path={APP_ROUTES.PRODUCT} element={<Product />} />
-              <Route path={APP_ROUTES.CHECKOUT} element={<Checkout />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
+              <Route element={<FeatureContextProvider />}>
+                <Route path={APP_ROUTES.PRODUCT} element={<Product />} />
+                <Route path={APP_ROUTES.CHECKOUT} element={<Checkout />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
+      </Suspense>
     </ErrorBoundary>
   );
 }
